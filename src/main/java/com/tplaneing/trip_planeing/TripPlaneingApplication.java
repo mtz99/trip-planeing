d18f03c02f47440dc6216e2780a46e4778080b39
Matthew Zhang <mtz99@users.noreply.github.com>
2025-06-19 18:29:14 -0700

    Adding the base files for the backend

---
+++ src/main/java/com/tplaneing/trip_planeing/TripPlaneingApplication.java
@@ -0,0 +1,13 @@
+package com.tplaneing.trip_planeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlaneingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlaneingApplication.class, args);
+	}
+
+}
