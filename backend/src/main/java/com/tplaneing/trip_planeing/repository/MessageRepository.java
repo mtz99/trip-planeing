9996d5cdd259398e80ff0e8f554171b2c6049327
Matthew Zhang <mtz99@users.noreply.github.com>
2025-07-17 23:16:53 -0700

    Trying this out

---
+++ backend/src/main/java/com/tplaneing/trip_planeing/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.tplaneing.trip_planeing.repository;
+
+import com.tplaneing.trip_planeing.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository <Message, Long> {
+}
